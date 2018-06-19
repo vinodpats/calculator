89d51aaab066c4bddd0742c54a4876af43d9041f
Patil Vinod <vinod.pats@gmail.com>
2018-06-19 14:38:59 +0000

    Initial commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,16 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller", "sap/m/MessageBox"
+], function(Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("com.pats.calculator.controller.View1", {
+			onAdd: function(){
+			var sum= parseInt(this.byId("inp1").getValue(),10) + parseInt(this.byId("inp2").getValue(),10);
+			MessageBox.information("Sum: "+ sum);
+			
+			
+		}
+	
 
 	});
 });
